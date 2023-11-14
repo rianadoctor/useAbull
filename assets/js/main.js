055b5af895da7ed32014d467bf41f2f62ce8e87a
rianadoctor <ridoctor@davidson.edu>
2023-11-14 16:10:04 -0500

    change to main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
 /* js */
 
 
+document.getElementById('button').addEventListener('click', function() {
+    // Get the body element
+    var body = document.body;
+
+    // Change background color
+    body.style.backgroundColor = 'lightblue';
+
+    // Hide the h1 element
+    document.getElementById('mainHeading').style.display = 'none';
+
+    // Create a new box element
+    var newBox = document.createElement('div');
+    newBox.style.width = '100px';
+    newBox.style.height = '100px';
+    newBox.style.backgroundColor = 'red';
+    newBox.style.position = 'absolute';
+    newBox.style.top = '50%';
+    newBox.style.left = '50%';
+    newBox.style.transform = 'translate(-50%, -50%)';
+    document.body.appendChild(newBox);
+});
